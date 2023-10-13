5de59652bc9e81e83047e5f48cff2c1d3565126e
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-14 00:56:10 +0530

    Create Convert String to LowerCase.js

---
+++ Easy Level Programs/JavaScript in Java Topic/Convert String to LowerCase.js
@@ -0,0 +1,9 @@
+class Solution{
+    toLower(str){
+        //converting the characters of the string to lowercase
+        let lowerStr = str.toLowerCase();
+        
+        //returning the converted string
+        return lowerStr;
+    }
+}
