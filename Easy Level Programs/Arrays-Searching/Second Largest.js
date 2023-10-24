53127ea050f2a782d05a8b787b9300b5c8ebde3c
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 01:03:15 +0530

    Create Second Largest.js

---
+++ Easy Level Programs/Arrays-Searching/Second Largest.js
@@ -0,0 +1,33 @@
+class Solution{
+    print2largest(arr,n){
+        let first = -Infinity;
+        let second = -Infinity;
+
+        // There should be at least two elements
+        if (n < 2) {
+            return -1;
+        }
+
+        for (let i = 0; i < n; i++) {
+
+            // If current element is greater
+            // than first then update both
+            // first and second
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            }
+
+            // If arr[i] is in between first
+            // and second then update second
+            else if (arr[i] > second && arr[i] != first) {
+                second = arr[i];
+            }
+        }
+        
+        if (second == -Infinity)
+            return -1;
+        else
+            return second;
+    }
+}
