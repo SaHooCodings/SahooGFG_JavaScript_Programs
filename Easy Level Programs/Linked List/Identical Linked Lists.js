63ad1e27315ed2a9f3f90975d885a84c923bc709
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 19:15:58 +0530

    Create Identical Linked Lists.js

---
+++ Easy Level Programs/Linked List/Identical Linked Lists.js
@@ -0,0 +1,14 @@
+class Solution {
+    //Function to check whether two linked lists are identical or not.
+    areIdentical(head1, head2)
+    {
+      while (head1 !== null && head2 !== null)      
+    	{ 
+    		if (head1.data !== head2.data)         
+    			return false;
+    		head1 = head1.next;
+    		head2 = head2.next;
+    	}
+    	return (head1 === null && head2 === null);    
+    }
+}
