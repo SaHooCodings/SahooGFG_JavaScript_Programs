6b9b374d41309d0c6f13626712da1183250af233
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 19:02:42 +0530

    Create Count nodes of linked list.js

---
+++ Easy Level Programs/Linked List/Count nodes of linked list.js
@@ -0,0 +1,13 @@
+class Solution {
+    getCount(head)
+    {
+        let count = 0;  
+        let current = head;
+        while (current !== null)
+        {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+}
