95744c815418c9ef78730ca75701fb1fff3ed709
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 15:33:15 +0530

    Create Upper case conversion.js

---
+++ Easy Level Programs/Strings/Upper case conversion.js
@@ -0,0 +1,13 @@
+class Solution{
+    transform(s){
+        let result = '';
+        for(let i=0; i<s.length; i++){
+            if(i === 0 || s[i-1] === ' '){
+                result += s[i].toUpperCase();
+            } else {
+                result += s[i];
+            }
+        }
+        return result;
+    }
+}
