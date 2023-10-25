3f5b99bd536a6db87a20d812e1c26b686bde90ed
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 20:18:16 +0530

    Create Remove loop in Linked List.js

---
+++ Medium Level Programs/Linked List/Remove loop in Linked List.js
@@ -0,0 +1,34 @@
+class Solution {
+    removeLoop(head)
+    {
+        let fast = head.next;
+        let slow = head;
+        
+        while( fast !== slow )
+        {
+            if( fast === null || fast.next === null ) 
+            return;
+            
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        let size = 1;
+        fast = fast.next;
+        while( fast !== slow )
+        {
+            fast = fast.next;
+            size++;
+        }
+        slow = head;
+        fast = head;
+        for(let i=0; i<size-1; i++)
+            fast = fast.next;
+        while( fast.next != slow )
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        fast.next = null;
+    }
+    
+}
